b6979a52362d0cbe07eb745c30e80ffa51faf199
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-01 21:54:56 +0200

    zapisywanie tabelki .js

---
+++ t-pro/src/tasks/task.js
@@ -0,0 +1,62 @@
+import React from 'react';
+// https://www.w3schools.com/tags/tag_thead.asp
+
+
+// wymyślić jakieś dane do tabelki
+// zakodować tabelke w normalnym html i wyświetlić ją w apce reacta
+
+const Table = ()=>{
+  return (<table border="5">
+  
+</table>)
+}
+// podzielić tabelkę na thead i tbody
+// umieścić thead w komponencie TableHead
+// umieścić tbody w komponencie TableBody
+// złożyć tabelkę w komponencie Table z componentów TableHead, TableBody
+// wyrenderować komponent Table w apce reacta
+
+const TableBody = ()=>{
+  
+  <TR>
+  <TD colspan="7"> <center><h1>head row</h1></center></TD> 
+  
+</TR>
+<TR> 
+    <TD>1 Standardowy Lorem</TD><TD> Ipsum</TD> <TD colspan="3">---error---none---#be empty</TD>
+</TR> 
+<TR border="2"> 
+<TD>2 @@@@@@@@</TD><TD>(- .-)</TD> <TD>0+0</TD><TD>bz zzz</TD><TD>(- .-)'</TD>
+</TR> 
+<TR border="8"> 
+<TD>3 @@@@@@@@@</TD><TD>(" - ")</TD> <TD>bz zz z</TD><TD>(- .-)</TD><TD>5</TD>
+</TR> 
+
+<TR> 
+<TD>4 @@@@@@@@</TD><TD>0+0</TD> <TD>bz zz z</TD><TD>(" - ")</TD><TD>(- .-)</TD>
+</TR>
+
+<TR> 
+<TD>5 @@@@@@@</TD><TD>(- .-)</TD> <TD>0+0</TD><TD>4</TD><TD>(- .-)</TD>
+</TR>
+
+<TR> 
+<TD>6 @@@@@@@</TD><TD>0+0</TD> <TD>(- .-)</TD><TD>bz zz z</TD><TD>(" - ")</TD>
+</TR>
+
+<TR> 
+<TD>7 @@@@@</TD><TD>(- .-)</TD> <TD>(- .-)</TD><TD>0+0</TD><TD>bz zz z</TD>
+</TR>
+
+<TR> 
+<TD>8 @@@@@</TD><TD>2</TD> <TD>(" - ")</TD><TD>p - p</TD><TD>(- .-)</TD>
+</TR>
+
+<TR> 
+<TD>9 @@@</TD><TD>(" - ")</TD> <TD>3</TD><TD>(- .-)</TD><TD>0+0</TD>
+</TR>
+<TR> 
+<TD>10 @@@@@@@</TD><TD>0+0</TD> <TD>(- .-)</TD><TD>bz zz z</TD><TD>(" - ")</TD>
+</TR>
+
+}
